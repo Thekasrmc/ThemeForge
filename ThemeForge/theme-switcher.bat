f068453db4e7b1fcd1ea5693de3fc9d7ea603161
TechAsRMC✨ <141753605+Thekasrmc@users.noreply.github.com>
2026-02-19 22:33:08 +0700

    Delete ThemeForge directory

--- ThemeForge/theme-switcher.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Request Admin Privileges and run PowerShell script in the same folder
-setlocal
-set "scriptPath=%~dp0theme-switcher.ps1"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoExit','-ExecutionPolicy','Bypass','-File','\"%scriptPath%\"' -Verb RunAs"
-endlocal
-exit /b
